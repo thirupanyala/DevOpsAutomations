46783a120a522f2195800e29f14523559830398b
Thirupathi Reddy Panyala <thirupanyala2k2@gmail.com>
2025-08-03 11:32:50 -0700

    Initial commit

---
+++ src/main/java/com/example/DevOpsAutomations/DevOpsAutomationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsAutomations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsAutomationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsAutomationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevOpsAutomations/DevOpsAutomationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsAutomations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsAutomationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
